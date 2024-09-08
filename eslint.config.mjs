cbd4784e80b1d3303c3194df9e6d430c7039b201
Cypro Freelance <110410268+Proxyy587@users.noreply.github.com>
2024-09-09 02:00:10 +0530

    maybe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,24 +3,11 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-export default [
-	{
-		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-		languageOptions: {
-			globals: globals.browser,
-		},
-		rules: {
-			"react/react-in-jsx-scope": "off",
 
-			"react/no-unescaped-entities": [
-				"error",
-				{
-					forbid: [">", '"', "}"],
-				},
-			],
-		},
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
